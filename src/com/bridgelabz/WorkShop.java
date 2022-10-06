8387e3dfc6a3c1dbf09963d3157a5eb98199cc3e
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-10-06 10:15:00 +0530

    Welcome to Workshop

---
+++ src/com/bridgelabz/WorkShop.java
@@ -0,0 +1,11 @@
+// Workshop 1st
+
+package com.bridgelabz;
+
+public class WorkShop
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Workshop");
+    }
+}
